c2986488e86a5c3c59fc0e075b257e5c66aedfd5
songoe <23y20302@pcs.hs.kr>
2023-05-10 17:01:16 +0900

    cat
    cat

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(-0.15f, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(0.15f, 0, 0);
+        }
+    }
+}
